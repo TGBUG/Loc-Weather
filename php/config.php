636b22f856f9ce2939e2cc75c8be6a0a278c7131
TGBUG <3770955706@qq.com>
2025-06-05 22:14:37 +0800

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+
+define("GAODE_KEY", "");
+define("WEATHERAPI_KEY", "");
+
+define("CACHE_DURATION_SECONDS", 600);  // 10 minutes
+define("RATE_LIMIT_MAX", 10);           // max 10 requests
+define("RATE_LIMIT_WINDOW", 60);        // in seconds
